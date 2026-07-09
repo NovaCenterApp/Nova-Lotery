bb9d7db126ac9577b864abf47e1d3209e2ed9b39
EquipoNC <127690424+NovaCenterApp@users.noreply.github.com>
2026-07-08 22:55:06 -0400

    Update BancaHome.jsx

--- BancaHome.jsx
+++ BancaHome.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { supabase } from './supabaseClient'
+import BancaListeros from './BancaListeros.jsx'
 
 export default function BancaHome() {
   const [alias, setAlias] = useState('')
